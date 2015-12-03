b2b084403762f4bb7632c18182aa3d179509cdd5
Nikolay Korotkiy <sikmir@gmail.com>
2015-12-04 02:19:48 +0300

    fixed ImportError

--- django_flickr_gallery/templatetags/flickr_tags.py
+++ django_flickr_gallery/templatetags/flickr_tags.py
@@ -1,4 +1,4 @@
-from django.template.base import Library
+from django.template import Library
 from django_flickr_gallery.models.album import FlickrAlbum
 
 from django_flickr_gallery.utils import FlickrPhotoIterator
