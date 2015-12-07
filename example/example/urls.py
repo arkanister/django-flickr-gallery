96452b1e70096bb89b75bbe42789878e2cb6caf8
Nikolay Korotkiy <sikmir@gmail.com>
2015-12-07 22:22:24 +0300

    fixed example

--- example/example/urls.py
+++ example/example/urls.py
@@ -2,9 +2,9 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = patterns('',
-    url(r'gallery/', include('django_flickr_gallery.urls')),
-    url(r'ckeditor/flickr/', include('flickr_ckeditor.urls')),
-    url(r'ckeditor/', include('ckeditor_uploader.urls')),
+    url(r'^gallery/', include('django_flickr_gallery.urls')),
+    url(r'^ckeditor/flickr/', include('flickr_ckeditor.urls')),
+    url(r'^ckeditor/', include('ckeditor_uploader.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
 )
